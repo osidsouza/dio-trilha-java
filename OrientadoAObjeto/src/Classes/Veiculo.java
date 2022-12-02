e49df7da857f9cb13a86f5946557158180671b74
Sid <s.p.souza@hotmail.com>
2022-12-02 00:00:05 -0300

    desafio poo

--- OrientadoAObjeto/src/Classes/Veiculo.java
+++ OrientadoAObjeto/src/Classes/Veiculo.java
@@ -1,4 +1,42 @@
 package Classes;
+import java.util.Stack;
+import java.util.Scanner;
 public class Veiculo {
+    public static void main(String[] args) {
+        
+
+        Scanner  c = new Scanner(System.in);
+
+       
+                    c.close();
+
+    }
     
-}
+    public static boolean isValid(char[] charArray) {
+        Stack<Character> container = new Stack<Character>();
+    
+        for (char c : charArray) {
+    
+            if (c == ' ') {
+                continue;
+            }
+    
+            if (c == '(' || c == '{' || c == '[') {
+                container.push(c);
+            } else if (c == ')' && container.peek() == '(' 
+                    || (c == '}' && container.peek() == '{')
+                    || (c == ']' && container.peek() == '[')) {
+                container.pop();
+            } else {
+                return false;
+            }
+          
+        }
+    
+        return container.isEmpty();
+    }
+    
+    }
+    
+    
+
